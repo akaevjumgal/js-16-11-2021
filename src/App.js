6f3063de9d9b182ecf3d92762c93546b25666db5
Nursultan34 <nursultanerikov7@gmail.com>
2022-02-22 19:50:20 +0600

    1234

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> fjklashfljafjlhahfa
+            Edit <code>src/App.js</code> fndksjbfsjlbfslbfsfsfs
           </p>
           <a
             className="App-link"
