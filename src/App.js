b91d03fed6f21ccdd0adcfb4ad64e8188f8ed1c4
Nursultan34 <nursultanerikov7@gmail.com>
2022-02-22 20:47:44 +0600

    fsafaf

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> fjklashfljafjlhahfa
+            Edit <code>src/App.js</code> fjklashfljagdgdgdlhahfa
           </p>
           <a
             className="App-link"
