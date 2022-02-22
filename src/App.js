782f36229c54fdee89bc8d1a11656b6bb5634d48
Nursultan34 <nursultanerikov7@gmail.com>
2022-02-22 19:56:04 +0600

    deploy3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> fndksjbfsjlbfslbfsfsfs
+            Edit <code>src/App.js</code> fndksjbfsjfbfslbfsfsfs
           </p>
           <a
             className="App-link"
