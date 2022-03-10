32b8f84d5d49de4112fc5a074a0f1f76eb444798
Islam52 <islamhihiza11@gmail.com>
2022-03-10 21:04:13 +0600

    fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-const App = () => {
-    return (  
-    <div>
-        <h1>hello stranger</h1>
-    </div>
-    );
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [posts, setPosts] = useState([])
+
+    const fetchPosts = async () => {
+        const response = await fetch('https://baconipsum.com/api/?type=meat-and-filler')
+        const result = await response.json()
+        setPosts(result)      
+    }
+    
+        useEffect(() => {
+            fetchPosts()
+            return () => {
+                <div>
+                    {[posts]}
+                </div>
+            }
+        }, []) 
 }
 
 export default App;
