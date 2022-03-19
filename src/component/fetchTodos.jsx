149d63d9d33a2cf5473d9ee04aff3733a40fa88b
Kasymov Marsel <marselkasymov23@gmail.com>
2022-03-19 19:09:30 +0600

    fetching

--- src/component/fetchTodos.jsx
+++ src/component/fetchTodos.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import './fetchTodos.css'
 
 export default function FetchTodos(){
-   const [posts,setPost] = useState({})
+   const [posts,setPost] = useState([])
     const fetchPost = async () => {
      const response = await fetch('https://baconipsum.com/api/?type=meat-and-filler')
      const result = await response.json()
@@ -10,29 +10,22 @@ export default function FetchTodos(){
     }
     useEffect(()=> {
         fetchPost()
+        
     },[])
+    console.log(posts)
      
   
-    return (
-        <div>
+    return( <div>
+        
         <div className="container">
-        <div className="Post_box">
-            <h3>{[posts].toString().split('.')[0]}</h3>
-        </div>
-        <div className="Post_box">
-            <h3>{[posts].toString().split('.')[1]}</h3>
-        </div>
-        <div className="Post_box">
-            <h3>{[posts].toString().split('.')[2]}</h3>
-        </div>
-        <div className="Post_box">
-            <h3>{[posts].toString().split('.')[3]}</h3>
-        </div>
-        <div className="Post_box">
-            <h3>{[posts].toString().split('.')[4]}</h3>
-        </div>
+        {posts.map((post)=> (
+
+            <div className="Post_box">
+                {post.split('.' )[0]}
+            </div>
+        ))}
         </div>
             <button className="post_btn" onClick={()=> fetchPost()}>Next</button>
-        </div>
-    )
+        </div>)
+    
 }
