8eee902db46f1403690beb4474d4bdd316cb38d1
ranbu171 <shulzhenko.dm@gmail.com>
2022-03-19 19:11:04 +0600

    asd

--- src/App.js
+++ src/App.js
@@ -8,15 +8,7 @@ import RenderProps from './Pages/RenderProps/RenderProps';
 
 function App() {
   return (
-    <>
-    <Header />
-    <Routes>
-    <Route path='/' element={<Home />} />
-      <Route path='albums' element={<ReactRouter />} />
-      <Route path='albums/:albumId' element={<AlbumDetail />} />
-      <Route path='renderProps' element={<RenderProps />} />
-    </Routes>
-    </>
+    <div></div>
   );
 }
 
